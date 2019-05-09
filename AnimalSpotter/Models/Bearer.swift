1747c7818e6a9b3cc9f00bd83fb467fc7188116c
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-09 12:19:03 -0400

    Set up model files

---
+++ AnimalSpotter/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by Lisa Sampson on 5/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
